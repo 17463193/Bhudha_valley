36845337e965e053ab8c48086acdbb4a2ad333cd
17463193 <tsheringtashi987@gmail.com>
2023-04-24 17:13:30 +0600

    latest

---
+++ Buddha-Valley/public/script.js
@@ -0,0 +1,40 @@
+//  searchForm
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick=()=>{
+   searchForm.classList.toggle('active');
+}//  searchForm
+
+let loginForm = document.querySelector('.login-form-container');
+document.querySelector("#login-btn").onclick = ()=>{
+   loginForm.classList.toggle('active');
+}
+document.querySelector("#close-login-btn").onclick = ()=>{
+   loginForm.classList.remove('active');
+}
+window.onscroll = () =>{
+   searchForm.classList.remove('active');
+   if(window.scrollY > 80){
+       document.querySelector('.header .header-2').classList.add('active');
+   }else{
+       document.querySelector('.header .header-2').classList.remove('active');
+   }
+}
+window.onload = () =>{
+   if(window.scrollY > 80){
+       document.querySelector('.header .header-2').classList.add('active');
+   }else{
+       document.querySelector('.header .header-2').classList.remove('active');
+   }
+}
+let AppointForm = document.querySelector('.appoint-form-container');
+document.querySelector("#appoint-btn").onclick = ()=>{
+ AppointForm.classList.toggle('active');
+}
+document.querySelector("#close-appoint-btn").onclick = ()=>{
+ AppointForm.classList.remove('active');
+}
+
+
+
+ 
---
+++ Buddha-Valley/resources/views/home.blade.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="font-awesome/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+   
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+    <title>Buddha valley</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+</head>
+<body>
+    <header class="header">
+        <div class="header-1">
+            <img src="bv.png">
+            <form action="" class="search-form">
+                <input type="search" name="" placeholder="search Here..." id="search-box">&nbsp &nbsp
+                <label for="search-box" class="fas fa-search"></label>
+            </form>
+           <a href="#" class="btn btn-success" style="font-size:12pt">Sign-out &nbsp<i class="fa fa-sign-out"></i></a>
+        </div>
+
+            <div class="header-2">
+                <nav class="navbar">
+                    <a href="#home">Home</a>
+                    <a href="#about">About Us</a>
+                    <a href="#video">Video Stream</a>
+                    <a href="#contact">Contact Us</a>
+                </nav>
+            </div>
+
+
+    </header>
+<!--header ends-->
+
+<!-- home secction starts -->
+<section class="home" id="home">
+    <div class="row">
+        <div class="content">
+            <h3>Welcome To Our Website</h3>
+            <p>Buddha Valley is a serene and tranquil webpage that exudes a sense of peace and calmness. 
+               The page is designed to inspire and encourage a sense of mindfulness,meditation, and 
+               spiritual growth. The color of scheme is soft and soothing with shades of green dominating the 
+               palette. The main header of the page features beautiful image of a peaceful Buddha statue, which
+               sets the tone for the rest of the page. As you scroll down, you will find a variety of resources
+               such as guided meditations, spiritual quotes and articles on mindfullness and Buddhism. It's a 
+               supportive online community that provides a wealth of resources and tools to support user's
+               spiritual growth and development. </p>
+            
+        </div>
+        <div class="swiper books-slider">
+            <div class="swiper-wrapper">
+            <a href="#" class="swiper-slide"><img src="yoga.jpg" alt=""></a>
+            <a href="#" class="swiper-slide"><img src="yoga1.jpg" alt=""></a>
+        </div>
+        
+        </div>
+    </div>
+    
+</section>
+<!-- icons section starts -->
+<center>
+<section class="icons-container">
+    <div class="icons">
+        <div class="content">
+            <i class="fa fa-street-view"></i><br>
+        <h3>Spiritual Teachings</h3>
+        </div>
+    </div>
+    <div class="icons">
+        <div class="content">
+            <i class="	fa fa-user-circle"></i><br>
+        <h3>Guided meditation</h3>
+        </div>
+    </div>
+    <div class="icons">
+        <div class="content">
+            <i class="fa fa-video-camera"></i><br>
+        <h3>Video Streaming</h3>
+        </div>
+    </div>
+    <div class="icons">
+        <div class="content">
+            <i class="	fa fa-universal-access"></i><br>
+        <h3>Daily Inspirations</h3>
+        </div>
+    </div>
+</section>
+</center>
+<!-- icons section ends -->
+<section>
+    <!-- outer card -->
+    <div class="outer-card">
+    <!-- Card 1 -->
+<div class="card">
+  <img src="spiritual.jpeg" alt="Image 1">
+  <h2>Spiritual Teaching:</h2>
+  <p>Spirituality in Bhutan is a left wing of the nation. Tiny nations like Bhutan has been 
+    still enjoying its independency in peace and harmony because of the strong spirituality of
+     people.</p>
+  <a href="#" class="button">Learn More</a>
+</div>
+
+<!-- Card 2 -->
+<div class="card">
+  <img src="guide.jpeg" alt="Image 2">
+  <h2>Guided Meditation:</h2>
+  <p>If you are looking to sharpen your mind, reconnect with your soul, 
+    and retake control of your life through the amazing life experience 
+    then this package was designed for you!</p>
+  <a href="#" class="button">Learn More</a>
+</div>
+
+<!-- Card 3 -->
+<div class="card">
+  <img src="video.jpg" alt="Image 3">
+  <h2>Video Stream:</h2>
+  <p>Meditation can lower your levels of stress and therefore have a positive effect on your body and mind. 
+    But the benefits of meditation go much deeper than relieving stress. 
+    </p>
+  <a href="#" class="button">Learn More</a>
+</div>
+
+<!-- Card 4 -->
+<div class="card">
+  <img src="daily.jpeg" alt="Image 4">
+  <h2>Daily Inspiration:</h2>
+  <p>Mindfulness refers to a state of awareness that stems from intentionally paying attention to your thoughts
+     in the present moment in a non-judgmental manner. Train your own mind!</p>
+  <a href="#" class="button">Learn More</a>
+</div>
+</div>
+</section>
+
+
+     <!--footer-->
+     <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>Company</h4>
+                    <ul>
+                        <li><a href="#">about us</a></li>
+                        <li><a href="#">your services</a></li>
+                        <li><a href="#">privacy policy</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Contact Us</h4>
+                    <ul>
+                        <li><a href="#">+975(17463193)</a></li>
+                        <li><a href="#">Athang pvt.ltd</a></li>
+                        <li><a href="#">Babena,Thimphu,Bhutan</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>Video Stream</h4>
+                    <ul>
+                        <li><a href="#">learn more</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>follow us </h4>
+                    <div class="social-links">
+                        <a href="https://www.facebook.com/profile.php?id=100091936606795" ><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+    <script src="script.js"></script>
+    
+</body>
+</html>
